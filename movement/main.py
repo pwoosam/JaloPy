70d5c892cf70c6f957472211ca8bb8ae7fe3188b
Patrick Woo-Sam <pwoosam5@icloud.com>
2018-01-03 10:37:16 +0000

    Added the video streamer and organized files

---
+++ movement/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import pygame
+import time
+import os
+import sys
+from motor_controller import MotorController
+from controller import Joystick
+
+if __name__ == '__main__':
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
+    pygame.init()
+    pygame.joystick.init()
+
+    motoCon = MotorController()
+    # if controllerJoystickIsUp:
+    #   motoCon.forward()
+    with Joystick(0) as controller:
+        while True:
+            controller.update()
+            inputs = controller.get_all_vals()
+            if inputs['button_0']:
+                motoCon.forward()
+            elif inputs['button_1']:
+                motoCon.stop()
+            elif inputs['button_2']:
+                motoCon.reverse()
+            motoCon.set_steering(inputs['axis_0'])
+            motoCon.print_stat()
+            time.sleep(0.1)
+            
